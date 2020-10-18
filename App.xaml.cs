ac4323e510206fb978e768d38646643978352789
nguyenq <nguyenq@users.sf.net>
2020-10-17 23:48:12 -0500

    Add command-line interface (CLI) support

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,33 +18,53 @@ namespace VietOCR
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int pid);
+     
         private string strRegKey = "Software\\VietUnicode\\";
         const string strUILang = "UILanguage";
         public const string strProgName = "VietOCR.NET";
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
+            if (e.Args.Length == 0)
+            {
+                base.OnStartup(e);
 
-            // Access registry to determine which UI Language to be loaded.
-            // The desired locale must be known before initializing visual components
-            // with language text. Waiting until OnLoad would be too late.
-            strRegKey += strProgName;
+                // Access registry to determine which UI Language to be loaded.
+                // The desired locale must be known before initializing visual components
+                // with language text. Waiting until OnLoad would be too late.
+                strRegKey += strProgName;
 
-            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(strRegKey);
+                RegistryKey regkey = Registry.CurrentUser.OpenSubKey(strRegKey);
 
-            if (regkey == null)
-                regkey = Registry.CurrentUser.CreateSubKey(strRegKey);
+                if (regkey == null)
+                    regkey = Registry.CurrentUser.CreateSubKey(strRegKey);
 
-            string selectedUILanguage = (string)regkey.GetValue(strUILang, "en-US");
-            regkey.Close();
+                string selectedUILanguage = (string)regkey.GetValue(strUILang, "en-US");
+                regkey.Close();
 
-            //XmlLanguage lang = XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag);
-            //FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(lang));
-            //FrameworkContentElement.LanguageProperty.OverrideMetadata(typeof(System.Windows.Documents.TextElement), new FrameworkPropertyMetadata(lang));
+                //XmlLanguage lang = XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag);
+                //FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(lang));
+                //FrameworkContentElement.LanguageProperty.OverrideMetadata(typeof(System.Windows.Documents.TextElement), new FrameworkPropertyMetadata(lang));
 
-            // Sets the UI culture to the selected language.
-            ChangeCulture(new CultureInfo(selectedUILanguage));
+                // Sets the UI culture to the selected language.
+                ChangeCulture(new CultureInfo(selectedUILanguage));
+            }
+            else
+            {
+                // Command line given, display console
+                if (!AttachConsole(-1)) // Attach to a parent process console
+                {
+                    AllocConsole(); // Alloc a new console
+                }
+
+                ConsoleApp.Main(e.Args);
+                Environment.Exit(0);
+            }
         }
 
         public static void ChangeCulture(CultureInfo newCulture)
---
+++ ConsoleApp.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VietOCR
+{
+    class ConsoleApp
+    {
+        public static void Main(string[] args)
+        {
+            new ConsoleApp().PerformOCR(args);
+        }
+
+        void PerformOCR(string[] args)
+        {
+            if (args[0] == "-?" || args[0] == "-help" || args.Length == 1 || args.Length >= 8)
+            {
+                Console.WriteLine("Usage: vietocr imagefile outputfile [-l lang] [-psm pagesegmode] [text|hocr|pdf|unlv|box|alto|tsv|lstmbox|wordstrbox] [postprocessing] [correctlettercases] [deskew] [removelines] [removelinebreaks]");
+                return;
+            }
+
+            FileInfo imageFile = new FileInfo(args[0]);
+            FileInfo outputFile = new FileInfo(args[1]);
+
+            if (!imageFile.Exists)
+            {
+                Console.WriteLine("Input file does not exist.");
+                return;
+            }
+
+            ProcessingOptions options = new ProcessingOptions();
+
+            HashSet<string> outputFormatSet = new HashSet<string>();
+            string[] RENDERERS = Enum.GetNames(typeof(Tesseract.RenderedFormat));
+
+            foreach (string arg in args)
+            {
+                // parse output formats
+                if (RENDERERS.Contains(arg.ToUpper()))
+                {
+                    outputFormatSet.Add(arg.ToUpper());
+                }
+
+                if ("postprocessing" == arg)
+                {
+                    options.PostProcessing = true;
+                }
+                if ("correctlettercases" == arg)
+                {
+                    options.CorrectLetterCases = true;
+                }
+                // pre- and post-processing
+                if ("deskew" == arg)
+                {
+                    options.Deskew = true;
+                }
+                if ("removelines" == arg)
+                {
+                    options.RemoveLines = true;
+                }
+                if ("removelinebreaks" == arg)
+                {
+                    options.RemoveLineBreaks = true;
+                }
+            }
+
+            if (outputFormatSet.Count == 0)
+            {
+                outputFormatSet.Add(Tesseract.RenderedFormat.TEXT.ToString());
+            }
+
+            string outputFormat = string.Join(",", outputFormatSet);
+            Console.WriteLine(outputFormat);
+
+            string curLangCode = "eng"; //default language
+            string psm = "3"; // or alternatively, "Auto"; // 3 - Fully automatic page segmentation, but no OSD (default)
+
+            if (args.Length == 4 || args.Length == 5)
+            {
+                if (args[2].Equals("-l"))
+                {
+                    curLangCode = args[3];
+                }
+                else if (args[2].Equals("-psm"))
+                {
+                    psm = args[3];
+                }
+            }
+            else if (args.Length == 6 || args.Length == 7)
+            {
+                curLangCode = args[3];
+                psm = args[5];
+                try
+                {
+                    Int16.Parse(psm);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input value.");
+                    return;
+                }
+            }
+
+            try
+            {
+                OCRHelper.PerformOCR(imageFile.FullName, outputFile.FullName, curLangCode, psm, outputFormat, options);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
