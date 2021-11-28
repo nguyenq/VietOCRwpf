997388f621d2a86be527f7cf92cf13dadd401eaf
Quan Nguyen <nguyenq@users.sf.net>
2021-11-28 13:21:41 -0600

    Increment version #

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("5.7.4.0")]
+[assembly: AssemblyFileVersion("5.8.0.0")]
