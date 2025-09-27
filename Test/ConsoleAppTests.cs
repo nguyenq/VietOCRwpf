ab78133303d952e025d9bad2e33846e2956ce95e
Quan Nguyen <nguyenq87@gmail.com>
2025-09-27 10:35:13 -0500

    Update Leptonica 1.86.0

---
+++ Test/ConsoleAppTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VietOCR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VietOCR.Tests
+{
+    [TestClass()]
+    public class ConsoleAppTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            string[] args = {"vietsample.tif", "out",  "-l", "vie", "pdf_textonly" };
+            ConsoleApp.Main(args);
+            //Assert.Fail();
+        }
+    }
+}
---
+++ Test/Utilities/PdfUtilitiesTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VietOCR.NET.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VietOCR.NET.Utilities.Tests
+{
+    [TestClass()]
+    public class PdfUtilitiesTests
+    {
+        [TestMethod()]
+        [Ignore]
+        public void ConvertPdf2TiffTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        [DeploymentItem("samples/vietsample1.pdf", "samples")]
+        public void GetPdfPageCountTest()
+        {
+            string inputPdfFile = "samples/vietsample1.pdf";
+            int expResult = 2;
+            int result = PdfUtilities.GetPdfPageCount(inputPdfFile);
+            Assert.AreEqual(expResult, result);
+        }
+    }
+}
